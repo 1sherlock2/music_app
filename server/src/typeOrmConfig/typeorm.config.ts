22efba06e9fc662188e124a4c4f0007a7c944525
1sherlock2 <alim.gazdiev@gmail.com>
2023-03-10 12:48:54 +0300

    fixed problems with docker

--- server/src/typeOrmConfig/typeorm.config.ts
+++ server/src/typeOrmConfig/typeorm.config.ts
@@ -4,24 +4,33 @@ import {
   TypeOrmModuleOptions
 } from '@nestjs/typeorm';
 
-const entities: string[] =
-  process.env.NODE_ENV === 'development'
-    ? ['./dist/**/*.entity.js']
-    : ['./**/*.entity.js'];
+const isDev = process.env.NODE_ENV === 'development';
+const entities: string[] = isDev
+  ? ['./dist/**/*.entity.js']
+  : ['./**/*.entity.js'];
 
 export default class TypeOrmConfig {
   static getOrmConfig(configService: ConfigService): TypeOrmModuleOptions {
+    console.log(
+      'DB_HOST',
+      configService.get(isDev ? 'DB_HOST' : 'DB_HOST_PROD')
+    );
     return {
       type: 'postgres',
-      host: configService.get('DB_HOST'),
-      port: configService.get('DB_PORT'),
+      host: configService.get(isDev ? 'DB_HOST' : 'DB_HOST_PROD'),
+      port: Number(configService.get('DB_PORT')),
       username: configService.get('DB_USERNAME'),
-      password: configService.get('DB_PASSWORD'),
+      password: String(
+        configService.get(isDev ? 'DB_PASSWORD' : 'DB_PASSWORD_PROD')
+      ),
       database: configService.get('DB_NAME'),
       entities: entities,
-      synchronize: process.env.NODE_ENV !== 'production', // false for migrations
+      synchronize: isDev, // false for migrations
+      autoLoadEntities: true,
+      verboseRetryLog: true,
       logging: true,
       migrationsRun: true,
+      // connectTimeoutMS: 2000,
       migrations: [__dirname + '/migrations/*{.ts,.js}'],
       cli: {
         migrationsDir: './migrations'
@@ -34,6 +43,7 @@ export const typeOrmConfigAsync: TypeOrmModuleAsyncOptions = {
   imports: [ConfigModule],
   useFactory: async (
     configService: ConfigService
-  ): Promise<TypeOrmModuleOptions> => TypeOrmConfig.getOrmConfig(configService),
+  ): Promise<TypeOrmModuleOptions> =>
+    await TypeOrmConfig.getOrmConfig(configService),
   inject: [ConfigService]
 };
